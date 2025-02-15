49586268637541d7839f59936fca9f812f8d3d2a
Belal Al Mazlom <b.mazlom@gmail.com>
2025-02-15 16:14:09 +1030

    Init app, packages, and layout of app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./app/**/*.{js,ts,tsx}', './src/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#001f3f',
+        secondary: '#003366',
+        tertiary: '#004080',
+        quaternary: '#0059b3',
+        quinary: '#0073e6',
+        black: '#000000',
+        white: '#ffffff',
+        gray: '#808080',
+        lightGray: '#d3d3d3',
+        darkGray: '#a9a9a9',
+        red: '#ff0000',
+        green: '#00ff00',
+      },
+    },
   },
   plugins: [],
 };
